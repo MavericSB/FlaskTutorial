f750ee7d679dc3cc00fc2f2cb1309e6e9ea9f2c5
Michael Love <michaellove88@gmail.com>
2022-07-02 16:54:29 -0600

    update titles, add posts to template

--- flaskblog.py
+++ flaskblog.py
@@ -3,15 +3,30 @@ from flask import Flask, escape, request, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Michael Love",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "July 2, 2022",
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "July 1, 2022",
+    },
+]
+
 
 @app.route("/")
 def hello():
-    return render_template("home.html")
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
